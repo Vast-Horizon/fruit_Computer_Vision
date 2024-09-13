dd90669e5e64534e61f5bba8aed6b1b46e42b124
Fiavi <fv2001007@gmail.com>
2024-09-13 18:57:11 -0400

    add a version check tool for pi

---
+++ pkg_versions.py
@@ -0,0 +1,13 @@
+"""
+This sctipt is for verifying the installtion of essential packages.
+"""
+import cv2
+import tflite_runtime.interpreter as tflite
+import numpy
+import matplotlib.pyplot as plt
+
+
+print("OpenCV version:", cv2.__version__)
+print("tflite_runtime version:", tflite.__version__)
+print("NumPy version:", np.__version__)
+print("Matplotlib version:", matplotlib.__version__)
