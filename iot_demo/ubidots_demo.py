f981dc8037d447cb7531506776662ffcb1213401
Fiavi <fv2001007@gmail.com>
2024-09-25 12:50:56 -0400

    add Tokens and labels

--- iot_demo/ubidots_demo.py
+++ iot_demo/ubidots_demo.py
@@ -1,26 +1,68 @@
-from ubidots import ApiClient
-import random
 import time
+import requests
+import math
+import random
 
-# Replace with your Ubidots Token
-UBIDOTS_TOKEN = "BBUS-zFNs6h6YSIb6EO1Bbk676Ab5thPCH6"
+TOKEN = "BBUS-zFNs6h6YSIb6EO1Bbk676Ab5thPCH6"  # Put your TOKEN here
+DEVICE_LABEL = "cybercart"  # Put your device label here
+VARIABLE_LABEL_1 = "numbers"  # Put your first variable label here
+VARIABLE_LABEL_2 = "humidity"  # Put your second variable label here
+VARIABLE_LABEL_3 = "position"  # Put your second variable label here
 
-# Initialize the API client
-api = ApiClient(token=UBIDOTS_TOKEN)
 
-# Replace 'your-device-label' and 'your-variable-label' with your specific labels
-device = api.get_device('cybercart')
-temperature_variable = device.get_variable('numbers')
+def build_payload(variable_1, variable_2, variable_3):
+    # Creates two random values for sending data
+    value_1 = random.randint(-10, 50)
+    value_2 = random.randint(0, 85)
 
-def send_data():
-    # Simulate temperature data or use a sensor to get real data
-    temperature = random.uniform(20.0, 30.0)
-    print(f"Sending temperature: {temperature:.2f}")
+    # Creates a random gps coordinates
+    lat = random.randrange(34, 36, 1) + \
+        random.randrange(1, 1000, 1) / 1000.0
+    lng = random.randrange(-83, -87, -1) + \
+        random.randrange(1, 1000, 1) / 1000.0
+    payload = {variable_1: value_1,
+               variable_2: value_2,
+               variable_3: {"value": 1, "context": {"lat": lat, "lng": lng}}}
 
-    # Send the data to Ubidots
-    temperature_variable.save_value({'value': temperature})
+    return payload
 
-if __name__ == "__main__":
-    while True:
-        send_data()
-        time.sleep(10)  # Send data every 10 seconds
+
+def post_request(payload):
+    # Creates the headers for the HTTP requests
+    url = "http://industrial.api.ubidots.com"
+    url = "{}/api/v1.6/devices/{}".format(url, DEVICE_LABEL)
+    headers = {"X-Auth-Token": TOKEN, "Content-Type": "application/json"}
+
+    # Makes the HTTP requests
+    status = 400
+    attempts = 0
+    while status >= 400 and attempts <= 5:
+        req = requests.post(url=url, headers=headers, json=payload)
+        status = req.status_code
+        attempts += 1
+        time.sleep(1)
+
+    # Processes results
+    print(req.status_code, req.json())
+    if status >= 400:
+        print("[ERROR] Could not send data after 5 attempts, please check \
+            your token credentials and internet connection")
+        return False
+
+    print("[INFO] request made properly, your device is updated")
+    return True
+
+
+def main():
+    payload = build_payload(
+        VARIABLE_LABEL_1, VARIABLE_LABEL_2, VARIABLE_LABEL_3)
+
+    print("[INFO] Attemping to send data")
+    post_request(payload)
+    print("[INFO] finished")
+
+
+if __name__ == '__main__':
+    while (True):
+        main()
+        time.sleep(1)
