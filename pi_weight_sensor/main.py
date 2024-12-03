df4f23c8ff8f38ad7a4f92fba43d9e1ea6422561
Fiavi <fv2001007@gmail.com>
2024-12-02 22:17:16 -0500

    testing

--- pi_weight_sensor/main.py
+++ pi_weight_sensor/main.py
@@ -95,8 +95,8 @@ def main_thread():
     weighting = Weighting(calibration_factor=0.00011765484757443882)
 
     # Enable simulation modes for testing
-    weight_sim = False
-    recog_sim = False
+    weight_sim = True
+    recog_sim = True
     weighting.testing_only(enable_simulation=weight_sim)
     recognition.enable_simulation(enable_simulation=recog_sim)
 
@@ -217,8 +217,8 @@ def main():
     weighting = Weighting(calibration_factor=0.00011765484757443882)
 
     # Enable simulation modes for testing
-    weight_sim = True
-    recog_sim = True
+    weight_sim = False
+    recog_sim = False
     weighting.testing_only(enable_simulation=weight_sim)
     recognition.enable_simulation(enable_simulation=recog_sim)
 
